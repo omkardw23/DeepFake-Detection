d8c0d0fdd984c7984c48d662afae64de6b5f5929
Omkar Watvisave <36334962+omkardw23@users.noreply.github.com>
2020-08-06 15:09:57 +0530

    Add files via upload
    Detailed MesoNet architecture

---
+++ MesoNet_Architecture/example.py
@@ -0,0 +1,33 @@
+import numpy as np
+from classifiers import *
+from pipeline import *
+
+from keras.preprocessing.image import ImageDataGenerator
+"""
+# 1 - Load the model and its pretrained weights
+classifier = Meso4()
+classifier.load('weights/Meso4_DF')
+
+# 2 - Minimial image generator
+# We did use it to read and compute the prediction by batchs on test videos
+# but do as you please, the models were trained on 256x256 images in [0,1]^(n*n)
+
+dataGenerator = ImageDataGenerator(rescale=1./255)
+generator = dataGenerator.flow_from_directory(
+        'test_images',
+        target_size=(256, 256),
+        batch_size=50,
+        class_mode='binary')
+
+
+# 3 - Predict
+X, y = generator.next()
+print('Predicted :', classifier.predict(X), '\nReal class :', y)"""
+
+# 4 - Prediction for a video dataset
+classifier = Meso4()
+classifier.load('weights/Meso4_F2F')
+
+predictions = compute_accuracy(classifier, 'test_videos')
+for video_name in predictions:
+    print('`{}` video class prediction :'.format(video_name), predictions[video_name][0])
